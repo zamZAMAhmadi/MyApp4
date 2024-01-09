99bc5432a2fb88dd3f330318bbf36a4c120e468a
Zamzama Ahmadi <zamzama2007@gmail.com>
2024-01-09 09:55:36 -0800

    hello

--- app/src/main/java/com/zz/myapp4/MainActivity.java
+++ app/src/main/java/com/zz/myapp4/MainActivity.java
@@ -1,8 +1,13 @@
 package com.zz.myapp4;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputConnection;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText editText = findViewById(R.id.editText);
+        editText.setShowSoftInputOnFocus(false);
+        editText.setRawInputType(InputType.TYPE_CLASS_TEXT);
+
+        InputConnection inputConnection = editText.onCreateInputConnection(new EditorInfo());
+        KeyboardCustomJava keyboardCustomJava = findViewById(R.id.keyboardCustom);
+        keyboardCustomJava.setInputConnection(inputConnection);
     }
 }
