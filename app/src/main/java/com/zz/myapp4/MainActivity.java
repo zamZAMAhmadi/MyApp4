12c12a5caa7cfc52b88f02f8390d112d04e96b24
Zamzama Ahmadi <zamzama2007@gmail.com>
2024-01-23 10:13:20 -0800

    hello

--- app/src/main/java/com/zz/myapp4/MainActivity.java
+++ app/src/main/java/com/zz/myapp4/MainActivity.java
@@ -3,25 +3,34 @@ package com.zz.myapp4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.text.InputType;
-import android.view.inputmethod.EditorInfo;
-import android.view.inputmethod.InputConnection;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button startB;
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.layout1);
 
-        EditText editText = findViewById(R.id.editText);
-        editText.setShowSoftInputOnFocus(false);
-        editText.setRawInputType(InputType.TYPE_CLASS_TEXT);
-
-        InputConnection inputConnection = editText.onCreateInputConnection(new EditorInfo());
-        KeyboardCustomJava keyboardCustomJava = findViewById(R.id.keyboardCustom);
-        keyboardCustomJava.setInputConnection(inputConnection);
+         startB = (Button) findViewById(R.id.startButton);
+         startB.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+                 openActivity2();
+             }
+         });
+    }
+    public void openActivity2() {
+        Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/zz/myapp4/SecondActivity.java
@@ -0,0 +1,16 @@
+package com.zz.myapp4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout2);
+    }
+
+
+}
